2361370ea375d0e4057ed5ef2d63df5ddb516ec6
keyestanga <keylinestanga@gmail.com>
2017-05-23 18:17:53 +0200

    FizzBuzz

---
+++ FizzBuzz/main.js
@@ -0,0 +1,14 @@
+for(var i=1; i<=100; i++){
+  var multiploTres= i%3===0;
+  var multiploCinco= i%5===0;
+
+  if(multiploTres && multiploCinco){
+    console.log("FizzBuzz");
+  }else if(multiploTres){
+    console.log("Fizz");
+  } else if(multiploCinco){
+     console.log("Buzz");
+   }else {
+     console.log(i);
+   }
+}
