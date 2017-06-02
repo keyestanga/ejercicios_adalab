5278136a77c531d64313ce27370f99fa33c1a36e
Key Estanga <keylinestanga@gmail.com>
2017-06-02 17:52:41 +0200

    use strict de main.js

--- FizzBuzz/main.js
+++ FizzBuzz/main.js
@@ -1,3 +1,4 @@
+'use strict';
 for(var i=1; i<=100; i++){
   var multiploTres= i%3===0;
   var multiploCinco= i%5===0;
@@ -7,8 +8,8 @@ for(var i=1; i<=100; i++){
   }else if(multiploTres){
     console.log("Fizz");
   } else if(multiploCinco){
-     console.log("Buzz");
-   }else {
-     console.log(i);
-   }
+    console.log("Buzz");
+  }else {
+    console.log(i);
+  }
 }
